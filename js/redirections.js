c290ec55d6ae10e1a2584113dd120089e4be722c
Emil <jeziorski.emil@gmail.com>
2020-03-10 21:46:19 +0100

    add link to heroku app

--- js/redirections.js
+++ js/redirections.js
@@ -1,11 +1,13 @@
 const urlTitles = `https://www.youtube.com/watch?v=ShV54CpAeEI`;
 const urlTemplate = `https://ecuki.github.io/Travel_template/`;
+const herokuOlaZuzia = `https://ola-zuzia.herokuapp.com/`;
 const titles = document.querySelector(".section__titles");
 const template = document.querySelector(".icon__template");
 const phone = document.querySelector(".icon__phone");
 const cat = document.querySelector(".icon__cat");
 const modal = document.getElementById("myModal");
 const exit = document.querySelector(".close");
+const caption = document.getElementById("caption");
 
 function openInNewTab(url) {
   var win = window.open(url, "_blank");
@@ -14,6 +16,10 @@ function openInNewTab(url) {
 
 titles.addEventListener("click", () => {
   openInNewTab(urlTitles);
+});
+
+caption.addEventListener("click", () => {
+  openInNewTab(herokuOlaZuzia);
 });
 
 template.addEventListener("click", () => {
